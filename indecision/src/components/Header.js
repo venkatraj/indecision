bf97d1e2c71d75217d9673195ca804531ea06958
Venkat <venkat@codinggeek.com>
2020-05-15 13:02:30 +0530

    header component style

--- indecision/src/components/Header.js
+++ indecision/src/components/Header.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Header = (props) => {
   const { title, subtitle } = props;
   return (
-    <div>
-      <h1>{title}</h1>
-      {subtitle && <h2>{subtitle}</h2>}
+    <div className="header">
+      <h1 className="header__title">{title}</h1>
+      {subtitle && <h2 className="header__subtitle">{subtitle}</h2>}
     </div>
   );
 };
